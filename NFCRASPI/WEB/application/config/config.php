d4c9b8c62ca277772445fa6dd4ef7b0017be1326
wepar2 <83143626+wepar2@users.noreply.github.com>
2022-02-25 14:03:34 +0100

    update

--- NFCRASPI/WEB/application/config/config.php
+++ NFCRASPI/WEB/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ilockpanel.tk/';
+$config['base_url'] = 'http://localhost/lock/';
 
 /*
 |--------------------------------------------------------------------------
